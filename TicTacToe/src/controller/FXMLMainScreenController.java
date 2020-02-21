c357a160318a794c57269a505f66bb6a59891b89
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-02-21 02:11:20 -0300

    Main screen controller development.

---
+++ TicTacToe/src/controller/FXMLMainScreenController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import main.Main;
+
+public class FXMLMainScreenController implements Initializable{
+	
+	@FXML
+    private Button onePlayerButton;
+
+    @FXML
+    private Button twoPlayerButton;
+
+    @FXML
+    private Button exitButton;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+	
+	@FXML
+	void onePlayerButtonEvent(ActionEvent event) {
+		Main.changeScreen("FXMLGameScreen");
+	}
+	@FXML
+	void twoPlayerButtonEvent(ActionEvent event) {
+		Main.changeScreen("FXMLGameScreen");
+	}
+	@FXML
+	void exitButtonEvent(ActionEvent event) {
+		System.exit(0);
+	}
+
+}
