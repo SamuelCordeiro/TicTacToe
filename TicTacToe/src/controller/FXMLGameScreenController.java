18694997ef3e14f175dfb4ba2c1b4e99e38a637e
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-02-26 01:05:30 -0300

    Game Screen controller update.

--- TicTacToe/src/controller/FXMLGameScreenController.java
+++ TicTacToe/src/controller/FXMLGameScreenController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
@@ -12,140 +13,93 @@ import main.Main;
 public class FXMLGameScreenController implements Initializable{
 
 	@FXML
-	private Button button00;
-
-	@FXML
-	private Button button01;
-
-	@FXML
-	private Button button02;
-
-	@FXML
-	private Button button10;
-
-	@FXML
-	private Button button11;
-
-	@FXML
-	private Button button12;
-
-	@FXML
-	private Button button20;
-
-	@FXML
-	private Button button21;
-
-	@FXML
-	private Button button22;
-
-	@FXML
-	private Button restartButton;
-
-	private boolean myTurn;
+	private Button button00, button01, button02, button10, button11, button12, button20, button21, button22, restartButton;
 	private String[][] ticTacToeTable;
+	private ArrayList<Button> buttonsList = new ArrayList<>();
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		loadGame();
-
 	}
 
 	@FXML
 	void button00Event(ActionEvent event) {
 		if(button00.getText() == "") {
-			button00.setText(playController());		
-			ticTacToeTable[0][0] = button00.getText();
-			if (GameController.winningChecker(ticTacToeTable)){
-				showVictory(GameController.key);
-			}
+			button00.setText(null);
+			button00.setText(GameController.playController(buttonsList, ticTacToeTable));		
+			updateGame();
 		}
 	}
 
 	@FXML
 	void button01Event(ActionEvent event) {
 		if(button01.getText() == "") {
-			button01.setText(playController());
-			ticTacToeTable[0][1] = button01.getText();
-			if (GameController.winningChecker(ticTacToeTable)){
-				showVictory(GameController.key);
-			}
+			button01.setText(null);
+			button01.setText(GameController.playController(buttonsList, ticTacToeTable));
+			updateGame();
 		}
 	}
 
 	@FXML
 	void button02Event(ActionEvent event) {
 		if(button02.getText() == "") {
-			button02.setText(playController());
-			ticTacToeTable[0][2] = button02.getText();
-			if (GameController.winningChecker(ticTacToeTable)){
-				showVictory(GameController.key);
-			}
+			button02.setText(null);
+			button02.setText(GameController.playController(buttonsList, ticTacToeTable));
+			updateGame();
 		}
 	}
 
 	@FXML
 	void button10Event(ActionEvent event) {
 		if(button10.getText() == "") {
-			button10.setText(playController());
-			ticTacToeTable[1][0] = button10.getText();
-			if (GameController.winningChecker(ticTacToeTable)){
-				showVictory(GameController.key);
-			}
+			button10.setText(null);
+			button10.setText(GameController.playController(buttonsList, ticTacToeTable));
+			updateGame();
 		}
 	}
 
 	@FXML
 	void button11Event(ActionEvent event) {
 		if(button11.getText() == "") {
-			button11.setText(playController());
-			ticTacToeTable[1][1] = button11.getText();
-			if (GameController.winningChecker(ticTacToeTable)){
-				showVictory(GameController.key);
-			}
+			button11.setText(null);
+			button11.setText(GameController.playController(buttonsList, ticTacToeTable));
+			updateGame();
 		}
 	}
 
 	@FXML
 	void button12Event(ActionEvent event) {
 		if(button12.getText() == "") {
-			button12.setText(playController());
-			ticTacToeTable[1][2] = button12.getText();
-			if (GameController.winningChecker(ticTacToeTable)){
-				showVictory(GameController.key);
-			}
+			button12.setText(null);
+			button12.setText(GameController.playController(buttonsList, ticTacToeTable));
+			updateGame();
 		}
 	}
 
 	@FXML
 	void button20Event(ActionEvent event) {
 		if(button20.getText() == "") {
-			button20.setText(playController());
-			ticTacToeTable[2][0] = button20.getText();
-			if (GameController.winningChecker(ticTacToeTable)){
-				showVictory(GameController.key);
-			}
+			button20.setText(null);
+			button20.setText(GameController.playController(buttonsList, ticTacToeTable));
+			updateGame();
 		}
 	}
 
 	@FXML
 	void button21Event(ActionEvent event) {
 		if(button21.getText() == "") {
-			button21.setText(playController());
-			ticTacToeTable[2][1] = button21.getText();
-			if (GameController.winningChecker(ticTacToeTable)){
-				showVictory(GameController.key);
-			}
+			button21.setText(null);
+			button21.setText(GameController.playController(buttonsList, ticTacToeTable));
+			updateGame();
 		}
 	}
 
 	@FXML
 	void button22Event(ActionEvent event) {
 		if(button22.getText() == "") {
-			button22.setText(playController());
-			ticTacToeTable[2][2] = button22.getText();
-			if (GameController.winningChecker(ticTacToeTable)){
-				showVictory(GameController.key);
-			}
+			button22.setText(null);
+			button22.setText(GameController.playController(buttonsList, ticTacToeTable));
+			updateGame();
 		}
 	}
 
@@ -153,48 +107,25 @@ public class FXMLGameScreenController implements Initializable{
 	void restartButtonEvent(ActionEvent event) {
 		Main.changeScreen("FXMLGameScreen");
 		loadGame();
-
-	}
-
-	private String playController() {
-		if(myTurn) {
-			myTurn = !myTurn;
-			return "X";	
-		}else {
-			myTurn = !myTurn;
-			return "O";	
-		}
-	}
-	
-	private void loadButtons() {
-
-		button00.setText("");
-		button01.setText("");
-		button02.setText("");
-		button10.setText("");
-		button11.setText("");
-		button12.setText("");
-		button20.setText("");
-		button21.setText("");
-		button22.setText("");
-		button00.setStyle("-fx-font: 40 arial; -fx-base: #b6e7c9;");
-		button01.setStyle("-fx-font: 40 arial; -fx-base: #b6e7c9;");
-		button02.setStyle("-fx-font: 40 arial; -fx-base: #b6e7c9;");
-		button10.setStyle("-fx-font: 40 arial; -fx-base: #b6e7c9;");
-		button11.setStyle("-fx-font: 40 arial; -fx-base: #b6e7c9;");
-		button12.setStyle("-fx-font: 40 arial; -fx-base: #b6e7c9;");
-		button20.setStyle("-fx-font: 40 arial; -fx-base: #b6e7c9;");
-		button21.setStyle("-fx-font: 40 arial; -fx-base: #b6e7c9;");
-		button22.setStyle("-fx-font: 40 arial; -fx-base: #b6e7c9;");
-		restartButton.setVisible(false);
 	}
 	
 	private void loadGame() {
-		loadButtons();
-		myTurn = true;
 		ticTacToeTable = new String [3][3];
+		loadButtonsList();
+		loadButtons();
+		loadTicTacToeTable();
+		GameController.myTurn = true;
 		disableButtons(false);
 	}
+	
+	private void loadButtons() {
+		for (int i = 0; i < buttonsList.size(); i++) {
+			buttonsList.get(i).setText("");
+			buttonsList.get(i).setStyle("-fx-font: 40 arial; -fx-base: #b6e7c9;");
+		}
+		restartButton.setVisible(false);
+	}
+	
 	public void showVictory(String key) {
 		reddeningButtons();
 		restartButton.setVisible(true);
@@ -246,33 +177,53 @@ public class FXMLGameScreenController implements Initializable{
 			button11.setStyle("-fx-base: #00FF00;");
 			button20.setStyle("-fx-base: #00FF00;");
 			break;
-		}	
+		}
+		case "draw":{
+			reddeningButtons();
+			break;
+		}
 		}
 		disableButtons(true);
 	}
 	
 	private void reddeningButtons() {
-
-		button00.setStyle("-fx-base: #FF0000;");
-		button01.setStyle("-fx-base: #FF0000;");
-		button02.setStyle("-fx-base: #FF0000;");
-		button10.setStyle("-fx-base: #FF0000;");
-		button11.setStyle("-fx-base: #FF0000;");
-		button12.setStyle("-fx-base: #FF0000;");
-		button20.setStyle("-fx-base: #FF0000;");
-		button21.setStyle("-fx-base: #FF0000;");
-		button22.setStyle("-fx-base: #FF0000;");
+		for (int i = 0; i < buttonsList.size(); i++) {
+			buttonsList.get(i).setStyle("-fx-base: #FF0000;");
+		}
 	}
 	
 	private void disableButtons(boolean disable) {
-		button00.setDisable(disable);
-		button01.setDisable(disable);
-		button02.setDisable(disable);
-		button10.setDisable(disable);
-		button11.setDisable(disable);
-		button12.setDisable(disable);
-		button20.setDisable(disable);
-		button21.setDisable(disable);
-		button22.setDisable(disable);
+		for (int i = 0; i < buttonsList.size(); i++) {
+			buttonsList.get(i).setDisable(disable);
+		}
+	}
+	
+	private void loadButtonsList() {
+		buttonsList.add(button00);
+		buttonsList.add(button01);
+		buttonsList.add(button02);
+		buttonsList.add(button10);
+		buttonsList.add(button11);
+		buttonsList.add(button12);
+		buttonsList.add(button20);
+		buttonsList.add(button21);
+		buttonsList.add(button22);
+	}
+	
+	private void updateGame() {
+		loadTicTacToeTable();
+		if (GameController.winningChecker(ticTacToeTable)){
+			showVictory(GameController.key);
+		}
+	}
+
+	private void loadTicTacToeTable() {
+		int temp = 0;
+		for(int i = 0; i < ticTacToeTable.length; i++) {
+			for (int j = 0; j < ticTacToeTable[0].length; j++) {
+				ticTacToeTable[i][j] = buttonsList.get(temp++).getText();
+			}
+		}
+		
 	}
 }
